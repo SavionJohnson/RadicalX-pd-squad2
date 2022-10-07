1b1138b93c762b2f4dd7fccb94361ed0b1d3f745
Wadood Abdul <97323719+WadoodAbdul25@users.noreply.github.com>
2022-10-07 02:29:11 -0400

    Add files via upload

--- auth/src/components/authentication/Dashboard/Cards.js
+++ auth/src/components/authentication/Dashboard/Cards.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Components_css/Cards.css"
+import copy_icon from '../../../images/Card_icons/copy.png'
+import edit_icon from '../../../images/Card_icons/edit.png'
+import trash_icon from '../../../images/Card_icons/trash.png'
 
 function Cards(props) {
-    const extendedRoleString = props.role.split(' ').slice(0,2).join(' ')+'...';
+    const extendedRoleString = props.role.split(' ').slice(0, 2).join(' ') + '...';
     return (
         <div className="card">
             <div className="topframe">
@@ -11,9 +14,11 @@ function Cards(props) {
                         <div className="text">
                             Mobile App Design
                         </div>
-                    </div>
-                    <div className="icons">
-
+                        <div className="icons">
+                            <button><img src={edit_icon} alt="edit" /></button>
+                            <button><img src={copy_icon} alt="edit" /></button>
+                            <button><img src={trash_icon} alt="edit" /></button>
+                        </div>
                     </div>
                 </div>
                 <div className="bottom">
@@ -23,11 +28,11 @@ function Cards(props) {
             <div className="bottomframe">
                 <div className="role">
                     <div className="text">
-                        {props.role.length >=17? extendedRoleString : props.role}
+                        {props.role.length >= 17 ? extendedRoleString : props.role}
                     </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }

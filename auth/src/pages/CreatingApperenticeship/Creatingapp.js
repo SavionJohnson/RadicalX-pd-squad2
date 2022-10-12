7af48b65d386c70b7256adbe9ee48029aedc17cb
Shayma Alkhafaf <79552804+shaymaalkhafaf@users.noreply.github.com>
2022-10-11 19:58:35 -0500

    Add files via upload

---
+++ auth/src/pages/CreatingApperenticeship/Creatingapp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Topbar/Header'
+import Navbar from '../../components/Topbar/Navbar'
+import Timeline from '../../components/Topbar/Timeline'
+
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <Navbar />
+    {/* <Timeline /> */}
+    </>
+  )
+}

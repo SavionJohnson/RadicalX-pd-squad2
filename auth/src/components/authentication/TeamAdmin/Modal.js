03983a63805da773a34d3fbc2165d06746613ae8
Wadood Abdul <97323719+WadoodAbdul25@users.noreply.github.com>
2022-10-14 19:20:46 -0400

    Add files via upload

--- auth/src/components/authentication/TeamAdmin/Modal.js
+++ auth/src/components/authentication/TeamAdmin/Modal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Modal.css'
 import close_icon from './icons/close.png'
 import profile_icon from './icons/profile.png'
+import picture_icon from './icons/blue_picture.png'
 
 export default function Modal(props) {
     if (!props.show) {
@@ -15,18 +16,26 @@ export default function Modal(props) {
                     <div className="card-title">Add Team Admin</div>
                     <div className="card-buttons">
                         <button id='save'>Save</button>
-                        <button className='close' onClick={props.onClose}><img src={close_icon} alt="X" /></button>
+                        <button className='icons' onClick={props.onClose}><img src={close_icon} alt="X" /></button>
                     </div>
                 </div>
 
-                <div className="icon"></div>
+                <div className="profile">
+                    <div className="picture-icon">
+                        <img src={picture_icon} alt="" />
+                    </div>
+                </div>
 
                 {/* input Form */}
-                <ul>
-                    <li>
-                        <label htmlFor="name"></label>
-                        {/* <i className='close'><img src={profile_icon} alt="" /></i> */}
+                <ul className='form'>
+                    <li className='input-form'>
                         <input type="name" className='input_text' placeholder='Name' />
+                    </li>
+                    <li className='input-form'>
+                        <input type="email" className='input_text' placeholder='Email Address' />
+                    </li>
+                    <li className='input-form'>
+                        <input type="Link" className='input_text' placeholder='LinkedIn URL (optional) ' />
                     </li>
                 </ul>
             </div>

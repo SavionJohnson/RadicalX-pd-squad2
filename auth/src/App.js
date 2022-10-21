9c6e0fec6f16d1136fbe683e8d824f89a07f5bf0
Shayma Alkhafaf <79552804+shaymaalkhafaf@users.noreply.github.com>
2022-10-21 10:02:41 -0500

    Add files via upload

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Login from './components/authentication/Login/Login';
 import SignUp from './components/authentication/SignUp/SignUp';
-import Dashboard from './components/authentication/Dashboard/Dashboard';
 import { Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import ProtectedRoute from './components/authentication/ProtectedRoute';
